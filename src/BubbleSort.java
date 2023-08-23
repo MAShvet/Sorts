68f00ff1aa2010e5a86b53c1192a145aff37caf9
MAShvet <shvetsov.mikhail.a@gmail.com>
2023-08-23 11:34:01 +0200

    Add BubbleSort

---
+++ src/BubbleSort.java
@@ -0,0 +1,19 @@
+public class BubbleSort
+{
+    public static int[] bubbleSort(int[] arr)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            for (int j = 0; j < (arr.length - 1 - i); j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int var = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = var;
+                }
+            }
+        }
+        return arr;
+    }
+}
